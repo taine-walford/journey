dab3610bd534385ba5c0bda9a57e3be0d3837083
Taine Walford <tainewalford@live.com>
2019-08-03 13:51:42 +1200

    React/Webpack setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from '../comps/App.jsx'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+})
